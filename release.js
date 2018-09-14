b58c61873400781572f9cf33f7789b0590650913
Cuong Kieu <kieucuong01@gmail.com>
2018-09-14 11:55:05 +0700

    add start

--- release.js
+++ release.js
@@ -6,10 +6,10 @@
 
 const options = {};
 
-options.ip = '127.0.0.1';
-options.port = parseInt(process.argv[2]);
-options.config = { name: 'Total.js' };
-options.sleep = 3000;
+// options.ip = '127.0.0.1';
+// options.port = parseInt(process.argv[2]);
+// options.config = { name: 'Total.js' };
+// options.sleep = 3000;
 
 require('total.js').http('release', options);
 // require('total.js').cluster.http(5, 'release', options);
